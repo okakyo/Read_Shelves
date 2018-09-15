83679b4ff2a10702f2a35411f5caace1fdf7acc0
Kyohei Oka <0622okakyo@gmail.com>
2018-09-15 09:41:54 +0900

    '修正中'

--- scraping/urls.py
+++ scraping/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
-from .views import Create_Article,Update_Article,Delete_Article,Article_View
+from .views import Create_Article,Update_Article,Delete_Article,Index_Article
 
-urlpatterns=()
+urlpatterns=(
+    path('',Index_Article.as_view(),name='index'),
+    path('/create',Create_Article.as_view(),name='create'),
+    path('/update/<int:pk>',Update_Article.as_view(),name='update'),
+    path('/delete',Delete_Article.as_view(),name='delete')
+)
--- scraping/views.py
+++ scraping/views.py
@@ -1,16 +1,48 @@
 from .models import  Article
-from django.views.generic import CreateView,DetailView,DeleteView,UpdateView
+from django.views.generic import CreateView,TemplateView,DeleteView,UpdateView
+from .scraping import get_article
 
-class Article_View(DetailView):
-    pass
+class Index_Article(TemplateView):
+    template_name = 'index.html'
+
+    def __init__(self):
+        super().__init__()
+        self.param={
+            'model':Article.objects.all()
+        }
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request,self.param)
+
 
 class Create_Article(CreateView):
-    pass
+    model = Article
+    template_name = 'create.html'
+
+
+    def get(self, request, *args, **kwargs):
+
+        return super().get(request,*args,**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request,*args,**kwargs)
+
 
 class Update_Article(UpdateView):
-    pass
+    model = Article
+    template_name = 'update.html'
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request,*args,**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 class Delete_Article(DeleteView):
-    pass
+    model = Article
+    template_name = 'delete.html'
+    success_url = '/delete'
+
 
 
